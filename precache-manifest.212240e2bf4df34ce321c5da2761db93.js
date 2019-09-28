85a8e5182699d251fdd2c39b78011c4ccc03eef5
jason <js@jason.com>
2019-09-28 14:03:57 -0500

    Updates

---
+++ precache-manifest.212240e2bf4df34ce321c5da2761db93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a73a5397dad408899af43402b787375",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "c88892e6c217375be897",
+    "url": "/my-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a8b310b8cb6d430a4486",
+    "url": "/my-app/static/js/2.1caea5c3.chunk.js"
+  },
+  {
+    "revision": "c88892e6c217375be897",
+    "url": "/my-app/static/js/main.dcbbff74.chunk.js"
+  },
+  {
+    "revision": "3a0f7d44cb8813799185",
+    "url": "/my-app/static/js/runtime-main.6277fce1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app/static/media/logo.25bf045c.svg"
+  }
+]);
